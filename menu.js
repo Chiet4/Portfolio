46a5f62b4bac36074b222424ed58a64cf7e82d9f
Anchieta Albano <111232477+Chiet4@users.noreply.github.com>
2025-08-12 12:16:15 -0300

    feat: adicionando lógica de clique para links em dispositivos móveis e removendo código desnecessário

--- menu.js
+++ menu.js
@@ -14,3 +14,27 @@ menuMobile.addEventListener('click', () => {
 overlayMenu.addEventListener('click', () => {
   menuMobile.classList.remove('active');
 });
+
+document.querySelectorAll('.img-port').forEach(item => {
+  let clickedOnce = false; // Variavel para lembrar a quantidade de clicks
+ 
+  item.addEventListener('click', function (e) {
+    const isMobile = window.matchMedia("(max-width: 989px)").matches; // Verifica se é mobile, pois muda a lógica
+
+    if (isMobile) {
+      if (!clickedOnce) {
+        // 1 toque no mobile somente mostra o overlay
+        e.preventDefault();
+        item.querySelector('.overlay').style.opacity = '1';
+        clickedOnce = true;
+        setTimeout(() => clickedOnce = false, 1500); // reseta se não clicar de novo em 1,5s 
+      } else {
+        // 2 toque então já abre o link
+        window.open(item.dataset.link, '_blank');
+      }
+    } else {
+      window.open(item.dataset.link, '_blank'); // Já abri direto, pois overlay aciona sem click
+    }
+  });
+});
+ 
