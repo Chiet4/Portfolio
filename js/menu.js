01af470ee8c3a7167ee2c02ddd188ae816bb6f0c
Anchieta Albano <111232477+Chiet4@users.noreply.github.com>
2025-08-13 15:46:10 -0300

    feat: adicionando versão final do portifolio

--- js/menu.js
+++ js/menu.js
@@ -1,25 +1,40 @@
-window.addEventListener("scroll", function () {
-  const menu = document.querySelector(".menu-fixo");
-  const scrollTop = window.scrollY;
-  const docHeight = document.documentElement.scrollHeight - window.innerHeight;
-  const scrollPercent = (scrollTop / docHeight) * 100;
 
-  let newColor = "";
+let btnMenu = document.getElementById('btn-menu');
+let menuMobile = document.getElementById('menu-mobile');
+let overlayMenu = document.getElementById('overlay-menu');
 
-  if (scrollPercent < 33) {
-    newColor = getComputedStyle(document.documentElement).getPropertyValue(
-      "--cor1"
-    );
-  } else if (scrollPercent < 66) {
-    newColor = getComputedStyle(document.documentElement).getPropertyValue(
-      "--cor3"
-    );
-  } else {
-    newColor = getComputedStyle(document.documentElement).getPropertyValue(
-      "--cor2"
-    );
-  }
-
-  menu.style.backgroundColor = newColor.trim();
+btnMenu.addEventListener('click', () => { 
+  menuMobile.classList.add('active');
 });
 
+menuMobile.addEventListener('click', () => {
+  menuMobile.classList.remove('active');
+});
+
+overlayMenu.addEventListener('click', () => {
+  menuMobile.classList.remove('active');
+});
+
+document.querySelectorAll('.img-port').forEach(item => {
+  let clickedOnce = false; // Variavel para lembrar a quantidade de clicks
+ 
+  item.addEventListener('click', function (e) {
+    const isMobile = window.matchMedia("(max-width: 989px)").matches; // Verifica se é mobile, pois muda a lógica
+
+    if (isMobile) {
+      if (!clickedOnce) {
+        // 1 toque no mobile somente mostra o overlay
+        e.preventDefault();
+        item.querySelector('.overlay').style.opacity = '1';
+        clickedOnce = true;
+        setTimeout(() => clickedOnce = false, 1500); // reseta se não clicar de novo em 1,5s 
+      } else {
+        // 2 toque então já abre o link
+        window.open(item.dataset.link, '_blank');
+      }
+    } else {
+      window.open(item.dataset.link, '_blank'); // Já abri direto, pois overlay aciona sem click
+    }
+  });
+});
+ 
